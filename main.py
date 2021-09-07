4b09d8df58100c85c4626fd482e49bf7e7c3897c
OmarHoussami <omar.houssami20@gmail.com>
2021-09-07 17:14:42 +0300

    setting the levels and lives position

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
 import os
-
 import time
 import random
+import math
+pygame.font.init()
 
-
-WIDTH, HEIGHT = 750, 750
-WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+WIDTH, HEIGHT = 750 ,750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
 
 #importing enemy ships assets
@@ -24,17 +24,26 @@ pygame.display.set_caption("Space Shooter")
 #LASER_PLAYER = pygame.image.load(os.path.join("assets", ""))
 
 #importing background
-BG = pygame.image.load(os.path.join("assets", "background.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background.png")), (WIDTH, HEIGHT))
 
 def main():
   run = True
   FPS = 160
   level = 1
   lives = 5
+  main_font = pygame.font.SysFont("comicsans", 45)
   clock = pygame.time.Clock()
   
   def refactor_window():
     WIN.blit(BG, (0, 0))
+    lives_label = main_font.render(f"lives: {lives}", 1, (224, 230, 227))
+    level_label = main_font.render(f"level: {level}", 1, (224, 230, 227))
+    
+   
+    WIN.blit(lives_label, (10, 10))
+    WIN.blit(level_label, (WIDTH - level_label.get_width() - 10 , 10))
+    
+    
     pygame.display.update()
   while run:
     clock.tick(FPS)
