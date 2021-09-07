ca8a1c3f9e62cca13ad02df53739db94d5673bc4
OmarHoussami <omar.houssami20@gmail.com>
2021-09-07 16:06:05 +0300

    loading assets

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import os
+import time
+import random
+
+#importing enemy ships assets
+ENEMEY_SHIP_ONE = pygame.image.load(os.path.join("assets", ""))
+LASER_SHIP_ONE = pygame.image.load(os.path.join("assets", ""))
+
+ENEMY_SHIP_TWO = pygame.image.load(os.path.join("assets", ""))
+LASER_SHIP_TWO = pygame.image.load(os.path.join("assets", ""))
+
+ENEMY_SHIP_THREE = pygame.image.load(os.path.join("assets", ""))
+LASER_SHIP_THREE = pygame.image.load(os.path.join("assets", ""))
+
+#importing player ship asset
+PLAYER_SHIP = pygame.image.load(os.path.join("assets", ""))
+LASER_PLAYER = pygame.image.load(os.path.join("assets", ""))
