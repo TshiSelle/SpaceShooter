858a931fd5a8a8412a7c7caf35d50c3a135da6b4
OmarHoussami <omar.houssami20@gmail.com>
2021-09-07 16:26:00 +0300

    event loop

--- main.py
+++ main.py
@@ -6,23 +6,38 @@ import time
 import random
 
 WIN = Tk()
-WIN.title('Space Shooter')
-WIN.geometry('1920x1080')
+#WIN.title('Space Shooter')
+#WIN.geometry('1920x1080')
 WIN.resizable(True, True)
+WIDTH, HEIGHT = 1920, 1080
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
 
 #importing enemy ships assets
-ENEMEY_SHIP_ONE = pygame.image.load(os.path.join("assets", ""))
-LASER_SHIP_ONE = pygame.image.load(os.path.join("assets", ""))
+#ENEMEY_SHIP_ONE = pygame.image.load(os.path.join("assets", ""))
+#LASER_SHIP_ONE = pygame.image.load(os.path.join("assets", ""))
 
-ENEMY_SHIP_TWO = pygame.image.load(os.path.join("assets", ""))
-LASER_SHIP_TWO = pygame.image.load(os.path.join("assets", ""))
+#ENEMY_SHIP_TWO = pygame.image.load(os.path.join("assets", ""))
+#LASER_SHIP_TWO = pygame.image.load(os.path.join("assets", ""))
 
-ENEMY_SHIP_THREE = pygame.image.load(os.path.join("assets", ""))
-LASER_SHIP_THREE = pygame.image.load(os.path.join("assets", ""))
+#ENEMY_SHIP_THREE = pygame.image.load(os.path.join("assets", ""))
+#LASER_SHIP_THREE = pygame.image.load(os.path.join("assets", ""))
 
 #importing player ship asset
-PLAYER_SHIP = pygame.image.load(os.path.join("assets", ""))
-LASER_PLAYER = pygame.image.load(os.path.join("assets", ""))
+#PLAYER_SHIP = pygame.image.load(os.path.join("assets", ""))
+#LASER_PLAYER = pygame.image.load(os.path.join("assets", ""))
 
 #importing background
 BG = pygame.image.load(os.path.join("assets", "background.png"))
+
+def main():
+  run = True
+  FPS = 160
+  clock = pygame.time.Clock()
+  
+  while run:
+    clock.tick(FPS)
+    
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
